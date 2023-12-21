86dc8b3c727b7bdc74b1310ec0137e7b53300b48
AnnPrim <Primpraowann@gmail.com>
2023-12-21 11:49:54 -0600

    added error handling

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -5,7 +5,14 @@
         message: document.getElementById("message").value
     }
 
-    emailjs.send("service_9idi6wb",
-                 "template_0bplghj",
-                 params).then(alert("Email was successfully sent"))
-}
+
+    emailjs.send('service_9idi6wb', 'template_0bplghj', params)
+    .then(function(response) {
+        alert("Email was successfully sent")
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        alert("Failed to send")
+        console.log('FAILED...', error);
+    });
+
+ };
